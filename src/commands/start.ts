b1874a6b0c7341f72b1f9407aacd83f7d386deb7
“BagusAK95” <bagus.ak95@gmail.com>
2019-02-19 14:44:40 +0700

    Fix Output

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,49 +1,9 @@
 import {Command} from '@oclif/command'
-import {IProject} from '../interfaces/main';
-import chalk from 'chalk'
-import Helper from '../../src/class/helper'
-const rp = require('request-promise')
-const cheerio = require('cheerio')
-const Table = require('cli-table')
 
 export default class Start extends Command {
-  private helper = new Helper()
-
   static description = 'start stealing data'
 
   async run() {
-    const project:IProject = this.helper.loadYaml(`./${this.helper.getProp('project')}.yml`)
-
-    const options = {
-      uri: project.target.uri,
-      timeout: project.target.timeout || 60000
-    }
-   
-    rp(options)
-      .then((html:any) => {
-        cheerio(project.parent.selector, html).each((i:any, elem:any) => {
-          this.log(`/* ${i + 1} */`)
-          const table = new Table({
-            head: [ 
-              chalk.blueBright('Content'), 
-              chalk.blueBright('Result'),
-            ],
-            colWidths: [10, 100]
-          });
-
-          project.childs.forEach(child => {
-            if (child.attribute) {
-              table.push([ child.content, cheerio(child.selector, elem).attr(child.attribute)])
-            } else {
-              table.push([ child.content, cheerio(child.selector, elem).text().trim()])
-            }
-          })
-
-          this.log(table.toString())
-        })
-      })
-      .catch((err:any) => {
-          this.error(err)
-      })
+    this.warn('This command is under development')
   }
 }
